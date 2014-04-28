a7914c33d82e9aeae536bef19d9a62fdd561198a
Adam <adam.gedney@gmail.com>
2014-04-28 10:36:43 -0400

    testing socketio settings

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  console.log("route picked up in /routes/index.js");
 });
 
 
