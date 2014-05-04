78b7d7ea93c961bde745cd33c68bdc00b0b30425
Adam <adam.gedney@gmail.com>
2014-05-04 16:52:02 -0400

    debugging users room joins

--- controllers/index.js
+++ controllers/index.js
@@ -67,6 +67,25 @@ module.exports.controller = function(app, io){
 
 
 
+			//========================================//
+			//Receives FORCE event from client emit
+			//========================================//
+			socket.on('force', function () {
+			  console.log("force");
+
+
+			  //Broadcast message to listening clients in room
+			  socket.broadcast.to(room).emit('roomForced');
+			});
+
+
+
+
+
+
+
+
+
 
 			//========================================//
 			//Receives PLAY event from client emit
